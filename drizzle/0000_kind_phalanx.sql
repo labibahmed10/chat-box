c0aafc19b72cad1cd5ac1deee0994df6928a74b3
labib ahmed <labib.ahmed.372@gmail.com>
2025-07-06 13:06:40 +0600

    fix: fixed dotenv, updated few other code

--- drizzle/0000_kind_phalanx.sql
+++ drizzle/0000_kind_phalanx.sql
@@ -4,6 +4,6 @@ CREATE TABLE `message` (
 	`receiver_id` varchar(255) NOT NULL,
 	`message` varchar(1000) NOT NULL,
 	`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	`updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	CONSTRAINT `message_id` PRIMARY KEY(`id`)
 );
--- src/Config/config.ts
+++ src/Config/config.ts
@@ -1,28 +1,32 @@
-import dotenv from "dotenv";
+import dotenvx from "@dotenvx/dotenvx";
 import path from "path";
 import mysql from "mysql2/promise";
 
-dotenv.config({path: path.join((process.cwd(), ".env"))});
+dotenvx.config({ path: path.join((process.cwd(), ".env")) });
+
+export const DB_Variables = {
+	host: process.env.DB_HOST as string,
+	user: process.env.DB_USER as string,
+	password: process.env.DB_PASSWORD as string,
+	database: process.env.DB_NAME as string,
+	port: Number(process.env.DB_PORT) as number,
+};
 
 async function createDBConnection() {
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: Number(process.env.DB_PORT),
-    });
+	try {
+		const connection = await mysql.createConnection({ ...DB_Variables });
 
-    await connection.connect();
-    console.log("✅ DB connection verified!");
-    return connection;
+		await connection.connect();
+		console.log("✅ DB connection verified!");
+		return connection;
+	} catch (error) {
+		console.error("❌ DB connection failed:", error);
+		throw error;
+	}
 }
 
 const config = {
-    port: process.env.PORT,
-}
+	port: process.env.PORT,
+};
 
-export {
-    config,
-    createDBConnection,
-}
+export { config, createDBConnection };
